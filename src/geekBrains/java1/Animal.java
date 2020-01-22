daa8552663eea92c84fc4ab1efaf80e9b2ad1fdc
arcanrun <arcan.run@gmail.com>
2020-01-22 03:44:15 +0300

    lesson06

--- src/geekBrains/java1/Animal.java
+++ src/geekBrains/java1/Animal.java
@@ -1,4 +1,22 @@
 package geekBrains.java1;
 
 public class Animal {
+
+    private String type;
+
+    Animal(String type) {
+        this.type = type;
+    }
+
+    public void run(int distance) {
+        System.out.println("The " + type + " is running a distance: " + distance);
+    }
+
+    public void swim(int distance) {
+        System.out.println("The " + type + " is swimming a distance: " + distance);
+    }
+
+    public void jump(int height) {
+        System.out.println("The " + type + " is jumping over: " + height);
+    }
 }
